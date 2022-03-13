fe2eb932d29a3a0860f61f0572124ecf161d5e25
GameTitan <saraf.siddharth@gmail.com>
2022-03-12 23:22:54 -0800

    Added modal functionality for contact form to show a response

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,11 @@
-// Code to make footer modal show
+// Code to make subscription modal show
 $("#subscribeForm").on("submit", function(e){
 	$('#subscribeModal').modal('show');
 	e.preventDefault();
+});
+
+// Code to make contect modal show
+$("#contactForm").on("submit", function(e){
+    $('#contactModal').modal('show');
+    e.preventDefault();
 });
