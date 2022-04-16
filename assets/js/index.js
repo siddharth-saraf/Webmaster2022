42ec45942d8b72623a9ebc451f02d6681e630cff
GameTitan <saraf.siddharth@gmail.com>
2022-04-15 20:43:29 -0700

    Added cfunctionality of showing slides to the side on the testimonial carousel, still need to improve on with overlap

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,5 +10,24 @@ $("#contactForm").on("submit", function(e){
     e.preventDefault();
 });
 
+// Code to make testimonial slideshow on past.html show more than one element per slide
+
+$("#testisCarousel .carousel-item").each(function() {
+	item = $(this);
+	next = item.next().children();
+	if (next.length == 0){
+		next = item.siblings().first().children().first();
+	};
+	secondNext = next.parent().next().children();
+	if (secondNext.length > 1) {
+		secondNext = secondNext.first();
+	} else if (secondNext.length < 1) {
+		secondNext = item.siblings().first().children().first();
+	};
+	next = next.clone().addClass('added')
+	secondNext = secondNext.clone().addClass('added')
+	item.append(next, secondNext);
+});
+
 // Giftbox
 // Used https://codepen.io/ElaineXu/pen/EWvGWX for reference
