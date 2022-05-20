0ce7b36c38216859d5e27638b7619a1c9455a98c
GameTitan <saraf.siddharth@gmail.com>
2022-05-19 19:03:02 -0700

    Finalized changes to make licensing on carousel be on top left corner of image

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,8 +10,38 @@ $("#contactForm").on("submit", function(e){
     e.preventDefault();
 });
 
-// Code to make testimonial slideshow on past.html show more than one element per slide
+// Hide all licensing 
+function hideLicensing() {
+	$('#mainCarousel .licensing').each(function() {
+		$(this).css('opacity', 0)
+	});
+};
 
+// Function to position licensing accurately on the home page Carousel called for each image on scroll
+function positionLicensing() { 
+	item = $('#mainCarousel .active');
+	leftPosition = item.find('img').offset().left - item.find('figure').offset().left;
+	topPosition = item.find('img').height - item.find('img').clientHeight;
+	imageReference = item.find('img');
+	image = imageReference[0];
+	sizeRatio = image.width / image.naturalWidth;
+	changeInHeight = ( image.height - sizeRatio * image.naturalHeight ) / 2;
+	item.find('figcaption.licensing').css('left', leftPosition);
+	item.find('figcaption.licensing').css('top', changeInHeight);
+	$('.active .licensing').css('opacity', 1);
+};
+
+function handleLicensing() {
+	hideLicensing();
+	positionLicensing();
+};
+
+handleLicensing();
+$( window ).resize( handleLicensing );
+$( '#mainCarousel' ).on('slid.bs.carousel', handleLicensing);
+
+
+// Code to make testimonial slideshow on past.html show more than one element per slide
 $("#testisCarousel .carousel-item").each(function() {
 	item = $(this);
 	next = item.next().children();
